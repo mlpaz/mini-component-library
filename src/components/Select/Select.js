4099230d9102c6fa897d020ce7c1a35e3e2cf668
Maximiliano Paz <mlpaz.code@gmail.com>
2024-04-20 13:57:03 -0300

    After view solution

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -5,62 +5,71 @@ import { COLORS } from "../../constants";
 import Icon from "../Icon";
 import { getDisplayedValue } from "./Select.helpers";
 
-function getTextWidth(inputText) {}
-
-const Select = ({ label, value, onChange, children }) => {
-  const displayedValue = getDisplayedValue(value, children);
+function getTextWidth(inputText) {
   const font = "16px 'Roboto', sans-serif";
-
   const canvas = document.createElement("canvas");
   const context = canvas.getContext("2d");
   context.font = font;
-  const widthNumber = context.measureText(displayedValue).width + 60;
-  const width = Math.ceil(widthNumber) + "px";
+  const widthNumber = context.measureText(inputText).width + 60;
+  return Math.ceil(widthNumber) + "px";
+}
+
+const Select = ({ label, value, onChange, children }) => {
+  const displayedValue = getDisplayedValue(value, children);
+  const width = getTextWidth(displayedValue);
   console.info(width);
 
   return (
     <Wrapper>
-      <SelectWrapper
+      <NativeSelect
         value={value}
         onChange={onChange}
         style={{ "--width": width }}
       >
         {children}
-      </SelectWrapper>
-      <ChevronDown id="chevron-down" size={24} strokeWidth={2} />
+      </NativeSelect>
+      <PrententionalSelect>
+        {displayedValue}
+        <SelectIcon id="chevron-down" size={24} strokeWidth={2} />
+      </PrententionalSelect>
     </Wrapper>
   );
 };
-
-const ChevronDown = styled(Icon)`
+const NativeSelect = styled.select`
   position: absolute;
-  top: 8px;
-  right: 8px;
-  z-index: -1;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  appearance: none;
 `;
 
-const SelectWrapper = styled.select`
-  width: var(--width);
-  -webkit-appearance: none;
-  -moz-appearance: none;
-  appearance: none;
+const PrententionalSelect = styled.div`
+  width: fit-content;
   padding: 12px 52px 12px 12px;
   color: ${COLORS.gray700};
   background-color: ${COLORS.transparentGray15};
   border-radius: 8px;
   border: none;
-  &:hover {
+  ${NativeSelect}:hover + & {
     color: ${COLORS.black};
+  }
+  ${NativeSelect}:focus + & {
+    outline: 1px dotted #212121;
+    outline: 5px auto -webkit-focus-ring-color;
   }
 `;
 
+const SelectIcon = styled(Icon)`
+  position: absolute;
+  top: 8px;
+  right: 8px;
+  pointer-events: none;
+`;
+
 const Wrapper = styled.div`
-  color: ${COLORS.gray700};
   position: relative;
   width: fit-content;
-  display: inline-block;
-  &:hover {
-    color: ${COLORS.black};
-  }
 `;
 export default Select;
